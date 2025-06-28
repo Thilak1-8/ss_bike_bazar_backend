394bc2477c0176d3cf8b413fb3f4d6fb3cf072e9
Thilak <murgamthilak@gmail.com>
2025-06-28 21:51:12 +0530

    renderlinked

--- index.js
+++ index.js
@@ -13,11 +13,8 @@ const app = express();
 const port = 5000;
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Needed for Render, Heroku, etc.
 });
 
 
